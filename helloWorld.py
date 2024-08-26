667aee365ef9e3a648f7b4e72d0c67c4f81734d4
GeneralMarshal <112048946+GeneralMarshal@users.noreply.github.com>
2024-08-26 14:28:17 +0100

    my first pythom push

---
+++ helloWorld.py
@@ -0,0 +1,30 @@
+# billTotal  = 310
+
+# discountOne = 10
+# discountTwo = 20
+
+
+# if billTotal >= 100 and billTotal < 200:
+#     print("bill is greater than 100")
+#     billTotal = billTotal - discountOne
+# elif billTotal >= 200:
+#     print("bill is greater than or equal to 200")
+#     billTotal = billTotal - discountTwo
+# else: 
+#     print("the bill is less than 100")
+
+# print("your total bill is: "  + str(billTotal))
+
+#Starter Code
+# favorites = ['Creme Brulee', 'Apple Pie', 'Churros', 'Tiramisú', 'Chocolate Cake']
+
+# for i in range(10):
+#     for j in range(10):
+#         print(0, end="")
+#     print("")
+
+def areaCalc( length, breadth):
+    area = round(length * breadth)
+    print("the area of this shape is ",area)
+
+areaCalc(17.6,27.0)
