21bda6666f4c71b16228e496c2f1e5b6e3aaf713
GeneralMarshal <112048946+GeneralMarshal@users.noreply.github.com>
2024-09-02 17:50:29 +0100

    reading files exercise

--- read_files.py
+++ read_files.py
@@ -1,6 +1,16 @@
+import random
 with open('newfile.txt', 'r') as file:
     print(file.read())
     # print(file.read(20))
     # print(file.readline())
     # print(file.readlines())
+
+
+# reading files exercise
+f =  open('testFile.txt')
+f_content = f.read()
+f_content_list = f_content.split("\n")
+f.close()
+
+print(random.choice(f_content_list))
 
